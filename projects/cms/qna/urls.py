99cb37d7dbd2dabbe56811829b21a68dd78e2914
Your Name <gymnopedies7@naver.com>
2020-12-23 17:32:34 +0900

    detail_html

--- projects/cms/qna/urls.py
+++ projects/cms/qna/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:question_id>/', views.detail),
 ]
--- projects/cms/qna/views.py
+++ projects/cms/qna/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("안녕하세요! ActionCam 관리시스템에 Q&A 입니다.")
+
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list':question_list}
+
+    return render(request, 'qna/question_list.html', context)
+
+def detail(request, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'qna/question_detail.html', context)
